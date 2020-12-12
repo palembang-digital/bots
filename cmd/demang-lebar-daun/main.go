236862362ab1f201aab1d4594220805788c11c88
agus mistiawan <agusmistiawan@192.168.1.5>
2020-12-13 04:58:09 +0700

    restructure project

--- cmd/demang-lebar-daun/main.go
+++ cmd/demang-lebar-daun/main.go
@@ -1,72 +1,35 @@
 package main
 
 import (
-	"context"
 	"log"
-	"net/http"
-	"time"
 
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/kelseyhightower/envconfig"
-	"google.golang.org/api/option"
-	"google.golang.org/api/sheets/v4"
+	"github.com/palembang-digital/bots/pkg/configs"
+	"github.com/palembang-digital/bots/pkg/googlesheet"
+	"github.com/palembang-digital/bots/pkg/telegram"
 )
 
-// Config stores configuration for the bot.
-type Config struct {
-	TelegramToken  string `required:"true" split_words:"true"`
-	TelegramChatID int64  `required:"true" split_words:"true"`
-	TelegramDebug  bool   `split_words:"true"`
-
-	SheetsCredentials      string `required:"true" split_words:"true"`
-	SheetsSpreadsheetID    string `required:"true" split_words:"true"`
-	SheetsSpreadsheetRange string `split_words:"true" default:"Telegram Bot"`
-}
-
 func main() {
-	ctx := context.Background()
-
-	var cfg Config
-	if err := envconfig.Process("", &cfg); err != nil {
-		log.Panic(err)
-	}
-
+	var cfg = configs.Get()
 	log.Println("Authenticating Telegram bot...")
-	bot, err := tgbotapi.NewBotAPI(cfg.TelegramToken)
+	bot, err := telegram.New(cfg.TelegramToken, cfg.TelegramChatID, cfg.TelegramDebug)
 	if err != nil {
 		log.Panic(err)
 	}
-	bot.Debug = cfg.TelegramDebug
-
 	log.Println("Getting Telegram chat members count...")
-	membersCount, err := bot.GetChatMembersCount(tgbotapi.ChatConfig{ChatID: cfg.TelegramChatID})
+	membersCount, err := bot.GetChatMembersCount()
 	if err != nil {
 		log.Panic(err)
 	}
-
 	log.Println("Telegram chat members count:", membersCount)
 
 	log.Println("Authenticating to Google Sheets...")
-	sheetsService, err := sheets.NewService(ctx, option.WithCredentialsJSON([]byte(cfg.SheetsCredentials)))
+	sheetsService, err := googlesheet.New(cfg.SheetsCredentials, cfg.SheetsSpreadsheetID, cfg.SheetsSpreadsheetRange)
 	if err != nil {
 		log.Panic(err)
 	}
-
-	log.Println("Writing to Google Sheets...")
-	valueRange := &sheets.ValueRange{
-		Values: [][]interface{}{{time.Now().UTC(), time.Now().Month(), membersCount}},
-	}
-	resp, err := sheetsService.Spreadsheets.Values.
-		Append(cfg.SheetsSpreadsheetID, cfg.SheetsSpreadsheetRange, valueRange).
-		ValueInputOption("RAW").
-		Do()
+	err = sheetsService.AppendMembersCount(membersCount)
 	if err != nil {
 		log.Panic(err)
 	}
-
-	if resp.ServerResponse.HTTPStatusCode != http.StatusOK {
-		log.Panic(resp.ServerResponse.HTTPStatusCode)
-	}
-
 	log.Println("Job completed! See ya~")
 }
---
+++ pkg/configs/config.go
@@ -0,0 +1,33 @@
+package configs
+
+import (
+	"log"
+	"sync"
+
+	"github.com/kelseyhightower/envconfig"
+)
+
+// Config stores configuration for the bot.
+type Config struct {
+	TelegramToken  string `required:"true" split_words:"true"`
+	TelegramChatID int64  `required:"true" split_words:"true"`
+	TelegramDebug  bool   `split_words:"true"`
+
+	SheetsCredentials      string `required:"true" split_words:"true"`
+	SheetsSpreadsheetID    string `required:"true" split_words:"true"`
+	SheetsSpreadsheetRange string `split_words:"true" default:"Telegram Bot"`
+}
+
+var conf Config
+var once sync.Once
+
+// Get returns the singleton config instance
+func Get() Config {
+	once.Do(func() {
+		err := envconfig.Process("", &conf)
+		if err != nil {
+			log.Fatal("Can't load config: ", err)
+		}
+	})
+	return conf
+}
